7d6a16de41ffa0355b625e7a66719fb39163a417
Josh Taylor <joshrtay@gmail.com>
2015-06-21 21:24:23 +0000

    passing first test

--- test/index.js
+++ test/index.js
@@ -2,5 +2,12 @@ var style = require('..')
 var assert = require('assert')
 
 describe('style', function () {
+  it('should return stringified styl', function () {
+    var styl = {
+      marginLeft: '10px',
+      flex: 1
+    }
 
+    assert.equal(style(styl), "margin-left:10px;flex:1;")
+  })
 })
